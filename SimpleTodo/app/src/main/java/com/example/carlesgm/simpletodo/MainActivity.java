642b90ba5233f7a614651673db06895d6c37451a
Enric Velasco <enric@Mac-mini-de-Enric.local>
2015-10-19 18:48:25 +0200

    simple todo

---
+++ SimpleTodo/app/src/main/java/com/example/carlesgm/simpletodo/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.carlesgm.simpletodo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayAdapter<String> adapter;
+    private EditText etTodo;
+    private ArrayList<String> items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, R.layout.row_layout, R.id.txtTodo, items);
+
+        ListView myList = (ListView) findViewById(R.id.lvTodos);
+        myList.setAdapter(adapter);
+
+        etTodo = (EditText) findViewById(R.id.etTodo);
+
+        myList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View view) {
+        adapter.add(String.valueOf(etTodo.getText()));
+        etTodo.setText("");
+    }
+}
